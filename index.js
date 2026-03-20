41cb67d55951f0cd58d402b816a4f1eaf02d70c1
satyambro03 <satyambro03@gmail.com>
2026-03-20 19:31:33 +0530

    Backend

---
+++ index.js
@@ -0,0 +1,31 @@
+// const express=require('express');
+// const app=express();
+
+// app.get("/",(req,res)=>{
+//     res.send("Satyam Vishwakarma I am SYBCA Student")
+// })
+
+// // app.listen(3000) only one time use post number other wise show the error
+// app.post("/",(req,res)=>{
+//     res.send("Satyam Vishwakarma I am SYBCA Student This is post method")
+// })
+
+// app.listen(3000,()=>{
+//     console.log("Server is Running on Port 3000");
+// })
+
+
+
+
+const express=require('express');
+const app=express();
+//("/","/get","/post","/Homepage","/contact" , etc) this is rule of set to focus this method 
+app.get("/get" ,(req,res)=>{
+    res.send("Hello Word In India!")
+})
+app.post("/post",(req,res)=>{
+    res.send("This is part of post method and it is very easy to understand")
+})
+app.listen(3000,()=>{
+console.log("Sever is active on part 3000 methon POST !")
+})
