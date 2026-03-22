106a598234bb6eadfc75cb0d1e4b1d20c3669dd7
satyambro03 <satyambro03@gmail.com>
2026-03-22 19:39:27 +0530

    Add Comment

--- index.js
+++ index.js
@@ -23,18 +23,24 @@ const app=express();
 app.get("/get" ,(req,res)=>{
     res.send("Hello Word In India!")
 })
-app.post("/post",(req,res)=>{
-    res.send("This is part of post method and it is very easy to understand")
-})
 
-app.delete("/delete",(req,res)=>{
-    res.send("This is Delete Method ")
-})
 
-app.patch("/patch",(req,res)=>{
-    res.send("This is Patch Method ")
-})
+
+// app.post("/post",(req,res)=>{
+//     res.send("This is part of post method and it is very easy to understand")
+// })
+
+// app.delete("/delete",(req,res)=>{
+//     res.send("This is Delete Method ")
+// })
+
+// app.patch("/patch",(req,res)=>{
+//     res.send("This is Patch Method ")
+// })
 // Mostly Four Methods are useing in API get,delete,post,patch 
+// Rest api hai jo http par run ho ta hia
+// get(),post(),delete(),patch() ye char method hai jo rest api me use hote hai
 app.listen(3000,()=>{
 console.log("Sever is active on part 3000 method POST !")
 })
+
