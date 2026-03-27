72f8b23bf51e3617a1f265c2dc7dce1ebf345db1
satyambro03 <satyambro03@gmail.com>
2026-03-27 20:10:57 +0530

    Method Add App.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req ,res)=>{
-    res.send("Hello World 😎 ")
+    res.send("This is Get Method Request 😎")
+})
+
+app.post('/post',(req,res)=>{
+    res.send("This is post Method Request 😎")
+})
+
+app.patch("/patch",(req,res)=>{
+    res.send("This is Patch Method Request 😎")
+})
+
+app.delete("/delete",(req,res)=>{
+    res.send("This is delete method request 😎")
 })
 module.exports =app
